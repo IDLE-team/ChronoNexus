c907627e20467a2e074d33094fc28bb76d8531c1
Yokomi <38473135+Yokomide@users.noreply.github.com>
2023-04-03 00:00:44 +0300

    EnemyAttack v0.2
    add: attack behavior improvements
    add: floor material
    chg: floor material

--- ChronoNexus/Assets/Scripts/Enemy/States/EnemyRangeAttackState.cs
+++ ChronoNexus/Assets/Scripts/Enemy/States/EnemyRangeAttackState.cs
@@ -1,10 +1,19 @@
+using System.Collections;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class EnemyRangeAttackState : EnemyState
 {
+    //ToDo Улучшить систему
+
+    private Transform _target;
     private Vector3 _playerPosition;
     private float shootingTimer = 0;
-    public float shootingInterval = 1f;
+    private float shootingInterval = 1f;
+    private float retreatDistance = 5f;
+    private float minDelay = 3f;
+    private float maxDelay = 4f;
+    private bool _isAttack = false;
 
     public EnemyRangeAttackState(Enemy enemy, StateMachine stateMachine) : base(enemy, stateMachine)
     {
@@ -12,13 +21,17 @@ public class EnemyRangeAttackState : EnemyState
 
     public override void Enter()
     {
-        _enemy.NavMeshAgent.stoppingDistance = 5f;
+        _target = _enemy.player.transform;
+        _enemy.NavMeshAgent.speed = 2.5f;
+        _isAttack = true;
+        ShootAndRetreat().Forget();
     }
 
     public override void Exit()
     {
-        _enemy.NavMeshAgent.stoppingDistance = 1f;
+        _isAttack = false;
         _enemy.canSeeTarget = false;
+        _enemy.NavMeshAgent.speed = 1.5f;
     }
 
     public override void LogicUpdate()
@@ -43,11 +56,30 @@ public class EnemyRangeAttackState : EnemyState
     public override void PhysicsUpdate()
     {
         _playerPosition = _enemy.player.position;
+        _target = _enemy.player.transform;
+    }
 
-        if (Vector3.Distance(_enemy.transform.position, _playerPosition) > 5)
+    private async UniTask ShootAndRetreat()
+    {
+        while (_isAttack)
         {
-            _enemy.NavMeshAgent.SetDestination(_playerPosition);
+            await UniTask.Delay((int)(Random.Range(minDelay, maxDelay) * 1000));
+
+            Vector3 randomDirection = Random.insideUnitSphere.normalized;
+            Vector3 retreatPosition = _target.position + randomDirection * retreatDistance;
+            retreatPosition = new Vector3(retreatPosition.x, _enemy.transform.position.y, retreatPosition.z);
+
+            float distanceToTarget = Vector3.Distance(retreatPosition, _target.position);
+            Debug.Log(distanceToTarget);
+            if (distanceToTarget < 5f)
+            {
+                retreatPosition += randomDirection * (5f - distanceToTarget);
+            }
+            if (Vector3.Distance(_enemy.transform.position, retreatPosition) > 0.1f)
+            {
+                _enemy.NavMeshAgent.SetDestination(retreatPosition);
+                await UniTask.Yield();
+            }
         }
-        else return;
     }
 }
