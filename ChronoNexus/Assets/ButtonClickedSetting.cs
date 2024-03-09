9acaee77775ccd9191493d62a3ad141fe689b7a8
Maksim Demian <80910641+DeadSunset@users.noreply.github.com>
2024-03-09 16:08:09 +0300

    New UI and functions for it

---
+++ ChronoNexus/Assets/ButtonClickedSetting.cs
@@ -0,0 +1,59 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickedSetting : MonoBehaviour
+{
+
+    public bool isClicked;
+    private SettignsButtons _parent;
+
+    private TextMeshProUGUI _text;
+    private Button _button;
+
+    [SerializeField] private Color _white = Color.white;
+    [SerializeField] private Color _black;
+
+    private void OnDrawGizmos()
+    {
+        _parent = GetComponentInParent<SettignsButtons>();
+        _button = gameObject.GetComponent<Button>();
+        _text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        if (!_parent)
+        {
+            _parent = GetComponentInParent<SettignsButtons>();
+        }
+        if (!_text)
+        {
+            _text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        }
+        PaintButton(isClicked);
+        _button.onClick.AddListener(Clicked);
+    }
+    public void Clicked()
+    {
+        var b = _parent.RecolorAllButtons(isClicked);
+        PaintButton(b);
+
+    }
+
+    public void PaintButton(bool clicked)
+    {
+        if (clicked)
+        {
+            _button.GetComponent<Image>().color = _white;
+            _text.color = _black;
+            isClicked = true;
+        }
+        else
+        {
+            _button.GetComponent<Image>().color = _black;
+            _text.color = _white;
+            isClicked = false;
+        }
+    }
+}
