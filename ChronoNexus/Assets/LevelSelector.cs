03ec2f2224722791304e2ed53d7d149132af4339
Yokomi <38473135+Yokomide@users.noreply.github.com>
2024-03-30 11:00:51 +0300

    Update
    fix: ui
    chg: level select

--- ChronoNexus/Assets/LevelSelector.cs
+++ ChronoNexus/Assets/LevelSelector.cs
@@ -12,7 +12,6 @@ public class LevelSelector : MonoBehaviour
     [SerializeField] private GameObject _lockScreen;
     [SerializeField] private Toggle _levelToggleButton;
     [SerializeField] private SceneLoader _sceneLoader;
-
     [SerializeField] private TextMeshProUGUI _killsText;
     [SerializeField] private TextMeshProUGUI _timeText;
 
@@ -38,16 +37,23 @@ public class LevelSelector : MonoBehaviour
         }
         if (!string.IsNullOrEmpty(_requiredLevelName))
         {
-            Debug.Log("TryOpen, required: " + _requiredLevelName);
-            Debug.Log("GetCleared: " + GetRequiredLevelCleared());
             if(GetRequiredLevelCleared())
                 Unlock();
         }
+        Debug.Log("isOn: " + _levelToggleButton.isOn);
     }
 
     private void OnDisable()
     {
         _levelToggleButton.onValueChanged.RemoveListener(OnToggleValueChanged);
+    }
+
+    private void Start()
+    {
+        if (_levelToggleButton.isOn)
+        {
+            OnToggleValueChanged(true);
+        }
     }
 
     private void OnToggleValueChanged(bool isOn)
@@ -69,7 +75,6 @@ public class LevelSelector : MonoBehaviour
 
         if (!string.IsNullOrEmpty(data))
         {
-            Debug.Log("Не Numll");
             string[] parts = data.Split(';');
 
             foreach (string part in parts)
@@ -81,7 +86,6 @@ public class LevelSelector : MonoBehaviour
                     {
                         string key = keyValue[0].Trim();
                         string value = keyValue[1].Trim();
-                        Debug.Log("Перед проверкой ключа");
                         if (key == "Cleared")
                         {
                             bool cleared = bool.Parse(value);
@@ -133,64 +137,7 @@ public class LevelSelector : MonoBehaviour
         }
 
     }
-    /*
-    public void LoadData(string levelName)
-    {
-        string data = PlayerPrefs.GetString(levelName);
-        if (data == null)
-            return;
-
-        string[] dataArray = data.Split(';');
-
-        bool cleared = false;
-        int kills = 0;
-        float time = 0;
-
-        foreach (string dataString in dataArray)
-        {
-            if (dataString.Contains("Kills"))
-            {
-                kills = int.Parse(dataString.Split(':')[1].Trim());
-            }
-            else if (dataString.Contains("Time"))
-            {
-                time = float.Parse(dataString.Split(':')[1].Trim().Split(':')[0]) * 3600 +
-                       float.Parse(dataString.Split(':')[1].Trim().Split(':')[1]) * 60 +
-                       float.Parse(dataString.Split(':')[1].Trim().Split(':')[2]);
-            }
-            else if (dataString.Contains("Cleared"))
-            {
-                cleared = bool.Parse(dataString.Split(':')[1].Trim());
-            }
-        }
-
-        if (cleared)
-        {
-            Debug.Log("Level cleared!");
-            Debug.Log("Kills: " + kills);
-            Debug.Log("Time: " + TimeSpan.FromSeconds(time).ToString(@"hh\:mm\:ss"));
-        }
-        else
-        {
-            Debug.Log("Level not cleared.");
-        }
-
-        
-        
-        _cleared = cleared;
-        _kills = kills;
-        _time = time;
-    
-    }
-*/
-    /*
-    private void SetTextStats()
-    {
-        _killsText.text = "Убийств\n" + _kills;
-        _timeText.text = "Время\n" +  TimeSpan.FromSeconds(_time).ToString(@"hh\:mm\:ss");
-    }
-    */
-private void Unlock()
+    private void Unlock()
     {          
         _isLocked = false;
         _levelToggleButton.interactable = true;
