4f84b7b20ac263d3404224b4f9e0f52e8d6da812
Yokomi <38473135+Yokomide@users.noreply.github.com>
2024-04-18 16:58:43 +0300

    Tutorial
    add: tutorial base

---
+++ ChronoNexus/Assets/TutorialController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _deactivationObjects;
+
+    public void StartControllerWork()
+    {
+        for (int i = 0; i < _deactivationObjects.Count; i++)
+        {
+            _deactivationObjects[i].SetActive(false);
+        }
+    }
+
+    public void EndControllerWork()
+    {
+        for (int i = 0; i < _deactivationObjects.Count; i++)
+        {
+            _deactivationObjects[i].SetActive(true);
+        }
+    }
+    
+}
