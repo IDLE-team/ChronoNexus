6bc078d56dce0c92923f89b58632fbf0fb20fc18
kitsunnet | Мацко Егор <76002087+fuett966@users.noreply.github.com>
2024-05-15 20:48:34 +0300

    pre boss

--- ChronoNexus/Assets/BossFightController.cs
+++ ChronoNexus/Assets/BossFightController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class BossFightController : MonoBehaviour
 {
@@ -9,6 +12,10 @@ public class BossFightController : MonoBehaviour
     [SerializeField] private AudioClip _endMusicBossFight;
     
     [SerializeField] private AudioSource _audioSource;
+    
+    [SerializeField] private GameObject _blackHole;
+    [SerializeField] private GameObject _bossGameObject;
+    [SerializeField] private PlayableDirector _timeline;
     
     public static Event OnBossFightStarted;
     public static Event OnBossFightEnded;
@@ -21,6 +28,33 @@ public class BossFightController : MonoBehaviour
     private void EndBossFight()
     {
         
+    }
+
+    public void SetEnableBoss(bool value)
+    {
+        _bossGameObject.SetActive(value);
+    }
+
+    public void SetEnableBlackHole(bool value)
+    {
+        if (value)
+        {
+            _blackHole.transform.DOScale(0, 0f);
+            _blackHole.SetActive(value);
+            _blackHole.transform.DOScale(10f, 2f);
+        }
+        else
+        {
+            _blackHole.transform.DOScale(0, 2f).OnComplete(()=>{_blackHole.SetActive(value);});
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _timeline.Play();
+        }
     }
 
 }
